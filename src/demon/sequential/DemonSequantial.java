f9a52ecbb76495bd84b24e9158ac27633e3b148d
Buğra Gedik <bgedik@gmail.com>
2015-06-14 16:28:22 +0300

    improved merge

--- src/demon/sequential/DemonSequantial.java
+++ src/demon/sequential/DemonSequantial.java
@@ -21,7 +21,7 @@ public class DemonSequantial {
          * change merge factor to see its effect. 1 means merge communities if
          * bigger community fully contains smaller community
          */
-        demon.execute(graphLoader.getNetwork(), 1.0, 0);
+        demon.execute(graphLoader.getNetwork(), 0.5, 0);
         System.out.println("Demon execution---> Done.");
         PrintWriter writer = new PrintWriter(new File("QuadraticOutput.txt"));
         writer.print(demon.getGlobalCommunities());
@@ -33,7 +33,7 @@ public class DemonSequantial {
         demon.setNumberOfComparison(0);
         averageConductance(graphLoader, demon); 
         System.out.println();
-        demon.execute(graphLoader.getNetwork(), 1.0, 1);
+        demon.execute(graphLoader.getNetwork(), 0.5, 1);
         System.out.println("Demon execution---> Done.");
         PrintWriter writer2 = new PrintWriter(new File("SubLinearOutput.txt"));
         writer2.print(demon.getGlobalCommunities());
